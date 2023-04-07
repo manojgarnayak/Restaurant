94608c93935696196695d3e03f70bb32508541c9
manojgarnayak <manojgarnayak07@gmail.com>
2023-04-07 19:03:54 +0530

    changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
 import RestaurantCard from "./Restaurantcard";
 import restaurantList from "../utils/mockdata";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const filterData = (searchInput, listofRestaurant) => {
     const filterData = listofRestaurant.filter((listofRestaurant) => {
@@ -12,8 +12,17 @@ const filterData = (searchInput, listofRestaurant) => {
 const Body = () => {
 
     const [listofRestaurant, setlistofRestaurant] = useState(restaurantList);
+    const [searchInput, setsearchInput] = useState("");
 
-    const [searchInput, setsearchInput] = useState();
+    useEffect(() => {
+         getRestaurant();
+    }, [])
+
+    async function getRestaurant(){
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING")
+        const json = await data.json();
+        setlistofRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    }
 
     return(
         <div className='body'>
